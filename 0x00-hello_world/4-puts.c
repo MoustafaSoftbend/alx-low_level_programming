706ecd739ccc43fe0f9542830f2191349868c26f
Mosben <bedib.moustafa@gmail.com>
2023-03-20 01:26:57 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 int main(void)
-/* This is the main function prints output and returns zero */
+/**
+ * main - Entry point
+ * 
+ * Description: 'function that prints output to the console'
+ * @parameter: no params
+ * 
+ * Return: Always 0 (Success)
+ */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
