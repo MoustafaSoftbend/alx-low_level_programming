7bad979ea587b365e6ddcd4fa2eb8d9976c16bca
Mosben <bedib.moustafa@gmail.com>
2023-03-22 02:46:53 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Return: Always 0 (Success)
+ */
+int print_alphabet_x10(void)
+{
+	for (i = 0; i < 10, i++)
+	{
+		print_alphabet();
+	}
+	return (0);
+}
+
+
