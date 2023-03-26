2177f3c8568ee281de3b807d44361d99c940ee50
Mosben <bedib.moustafa@gmail.com>
2023-03-26 03:53:09 -0700

    Learn your times table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,7 @@ int _islower(int c);
 int _isalpha(int c);
 int _abs(int);
 void jack_bauer(void);
+void times_table(void);
 int print_last_digit(int);
 int print_sign(int n);
 void print_alphabet(void);
