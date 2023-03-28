247ecf6758450a890f6b28c9928ae549b8fc0dab
Mosben <bedib.moustafa@gmail.com>
2023-03-28 01:33:08 -0700

    98 Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point for the program
+ * reset_to_98 - Entry point for the program
  *
  * Return: Always 0 (Success)
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 	return (0);
