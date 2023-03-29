f0233f74cc05d66bc53176dafd27e590e5464d52
Mosben <bedib.moustafa@gmail.com>
2023-03-29 08:54:37 -0700

    believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-
 /**
  * print_most_numbers - prints the numbers from 0 to 9, except 2 and 4, followed by a new line
+ *
+ * @params: void 
  */
 void print_most_numbers(void)
 {
