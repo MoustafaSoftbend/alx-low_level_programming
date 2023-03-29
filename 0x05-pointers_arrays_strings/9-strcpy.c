11160c515c007a2cb5d325057f95ed321171a035
Mosben <bedib.moustafa@gmail.com>
2023-03-29 01:31:11 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,10 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    char *p = dest;
-    while (*src != '\0') {
-        *p++ = *src++;
-    }
-    *p = '\0';
-    return dest;
+	char *p = dest;
+
+	while (*src != '\0')
+		*p++ = *src++;
+	*p = '\0';
+	return (dest);
 }
