688a6632808b0981807015c4bd59f33f0067eccd
Mosben <bedib.moustafa@gmail.com>
2023-03-30 03:53:46 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 if s1 and s2 are equal, negative integer if s1 is less than s2,
+ *         positive integer if s1 is greater than s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+    int i;
+
+    for (i = 0; s1[i] && s2[i]; i++)
+    {
+        if (s1[i] != s2[i])
+            return (s1[i] - s2[i]);
+    }
+
+    return (s1[i] - s2[i]);
+}
