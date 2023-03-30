99a4f52022362c64e7c83012576dfede748cbf28
Mosben <bedib.moustafa@gmail.com>
2023-03-30 03:03:51 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,19 +12,16 @@ char *_strcat(char *dest, char *src)
 {
 	char *ptr = dest;
     
-	// Find the end of the destination string
 	while (*ptr != '\0') 
 	{
 		ptr++;
 	}
     
-	// Append the source string to the destination string
 	while (*src != '\0') 
 	{
 		*ptr++ = *src++;
 	}
     
-	// Add the terminating null byte
 	*ptr = '\0';
     
 	return (dest);
