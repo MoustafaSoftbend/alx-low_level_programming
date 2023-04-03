8e7d0be236a6b3f1a344b001929b32c6b5352445
Mosben <bedib.moustafa@gmail.com>
2023-04-03 03:51:51 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,19 +11,19 @@
  */
 char *_strchr(char *s, char c)
 {
-        while (*s != '\0')
-        {
-                if (*s == c)
-                {
-                        return (s);
-                }
-                s++;
-        }
-
-        if (*s == c)
-        {
-                return (s);
-        }
-
-        return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	
+	if (*s == c)
+	{
+		return (s);
+	}
+	
+	return (NULL);
 }
